7bf81c3c8f6414168b22f1f9833f327c2399e6b8
Neetbear <flffkry@gmail.com>
2022-04-30 22:15:29 +0900

    0430
    오타 수정

--- 220428/main.js
+++ 220428/main.js
@@ -5,7 +5,7 @@
 import { initHttpServer } from "./httpServer.js";
 import { initP2PServer } from "./p2pServer.js";
 
-// 포드등의 숫자 코드에 직접 보여주는건 프로그래머만 -> env(환경변수)에서 제어할 수 있게 짜주는게 관리에 용이
+// 포트등의 숫자 코드에 직접 보여주는건 프로그래머만 -> env(환경변수)에서 제어할 수 있게 짜주는게 관리에 용이
 const httpPort = parseInt(process.env.HTTP_PORT) || 3001; 
 const p2pPort = parseInt(process.env.P2P_PORT) || 6001; 
 
