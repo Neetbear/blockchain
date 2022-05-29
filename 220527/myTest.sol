b090ec5df7f082d5078befeba8db16c3eda00460
Neetbear <flffkry@gmail.com>
2022-05-29 17:11:41 +0900

    0529

--- 220527/myTest.sol
+++ 220527/myTest.sol
@@ -12,12 +12,21 @@ contract MyTestContract {
         return msg.sender.balance;
     }
 
-    function transferBalance(address _to, uint256 _amount) public {
+    function transferBalance(address payable _to) public payable {
+        // 송금
+        require(msg.sender.balance >= msg.value);
+        _to.transfer(msg.value);
+        // bool sent = payable(_to).send(_amount);
+        // require(sent, "transfer failed");
+        emit Transfer(msg.sender, _to, msg.value);
+    }
+
+    function transferBalance2(address payable _to, uint _amount) public payable {
         // 송금
         require(msg.sender.balance >= _amount);
-        // msg.sender.transfer(_amount);
-        bool sent = payable(_to).send(_amount);
-        require(sent, "transfer failed");
+        _to.transfer(_amount);
+        // bool sent = payable(_to).send(_amount);
+        // require(sent, "transfer failed");
         emit Transfer(msg.sender, _to, _amount);
     }
 }
